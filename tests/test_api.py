e541070f14a8f9456a1f695a50a90c4e7d2c1140
redatman <evicwork@gmail.com>
2024-06-18 01:16:57 +0800

    test: Improve Simplenote API tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,55 +1,105 @@
 import logging
+import time
 from typing import Any
 from unittest import TestCase, main
 
-from api import Note, NoteWrapper, Simplenote
+from api import Simplenote
 from config import CONFIG
 
 
 logger = logging.getLogger()
 logger.info(CONFIG)
 
+_note_kwargs: dict[str, Any] = {
+    "tags": [],
+    "deleted": False,
+    "shareURL": "",
+    "systemTags": [],
+    "content": "SimplenoteTitle\n\n\nSimplenoteBody",
+    "publishURL": "",
+    "modificationDate": 1539088227.272,
+    "creationDate": 1529054831.73117,
+}
 
-class TestNote(TestCase):
-    _note_kwargs: dict[str, Any] = {
-        "tags": ["tag1", "tag2"],
-        "deleted": False,
-        "shareURL": "",
-        "systemTags": ["system_tag1", "system_tag2"],
-        "content": "SimplenoteTitle\n\n\nSimplenoteBody",
-        "publishURL": "",
-        "modificationDate": 1539088227.272,
-        "creationDate": 1529054831.73117,
-    }
-    _d_kwargs: dict[str, Any] = {
-        "d": _note_kwargs,
-        "id": "123abc",
-        "v": 2,
-    }
-
-    def test_note(self):
-        note = Note(**self._note_kwargs)
-        assert note.content == "SimplenoteTitle\n\n\nSimplenoteBody"
-        assert note.tags == ["tag1", "tag2"]
-        assert note.systemTags == ["system_tag1", "system_tag2"]
-        assert note.deleted is False
-
-    def test_note_wrapper(self):
-        note = NoteWrapper(**self._d_kwargs)
-        assert note.id == "123abc"
-        assert note.v == 2
-        assert isinstance(note.d, Note)
-        assert note.d.content == "SimplenoteTitle\n\n\nSimplenoteBody"
-        logger.info(note.__dict__)
+_d_kwargs: dict[str, Any] = {
+    "d": _note_kwargs,
+    "id": "123abc",
+    "v": 2,
+}
 
 
 class TestSimplenoteApi(TestCase):
-    def setUp(self):
-        self.instance = Simplenote(CONFIG.SIMPLENOTE_USERNAME, CONFIG.SIMPLENOTE_PASSWORD)
+    def __get_random_note_id(self):
+        status, result = self.API.index(limit=1, data=True)
+        assert status == 0
+        assert isinstance(result, dict)
+        logger.info((status, result))
+        note_id = result["index"][0]["id"]
+        assert isinstance(note_id, str)
+        logger.info(note_id)
+        return note_id
 
-    def test_get_note_list(self):
-        note_list, status = self.instance.get_note_list()
-        logger.info((status, note_list))
+    def setUp(self):
+        self.API = Simplenote(CONFIG.SIMPLENOTE_USERNAME, CONFIG.SIMPLENOTE_PASSWORD)
+
+    def test_authenticate(self):
+        result = self.API.authenticate(CONFIG.SIMPLENOTE_USERNAME, CONFIG.SIMPLENOTE_PASSWORD)
+        assert isinstance(result, str)
+        logger.info((result))
+
+    def test_token(self):
+        token = self.API.token
+        assert isinstance(token, str)
+        logger.info(token)
+
+    def test_create(self):
+        _note_kwargs["content"] = "New note content at %s" % time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        _note_kwargs["creationDate"] = time.time()
+        _note_kwargs["modificationDate"] = time.time()
+        status, note_d = self.API.modify(_note_kwargs)
+        logger.info((status, note_d))
+        assert status == 0
+
+    def test_index(self):
+        status, result = self.API.index(limit=1, data=True)
+        assert isinstance(result, dict)
+        logger.info((status, result))
+        assert status == 0
+
+    def test_retrieve(self):
+        status, note_d = self.API.retrieve(self.__get_random_note_id())
+        assert isinstance(note_d, dict)
+        logger.info((status, note_d))
+        assert status == 0
+
+    def test_modify(self):
+        random_note_id = self.__get_random_note_id()
+        status, random_note_d = self.API.retrieve(random_note_id)
+        logger.info(random_note_d)
+        assert status == 0
+        assert isinstance(random_note_d, dict)
+        _time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        random_note_d["content"] = f"Updated content at {_time}"
+        status, note = self.API.modify(random_note_d, random_note_id)
+        logger.info((status, note))
+        assert status == 0
+
+    def test_delete(self):
+        random_note_id = self.__get_random_note_id()
+        status, note_d = self.API.retrieve(random_note_id)
+        assert status == 0
+        assert isinstance(note_d, dict)
+        status, note = self.API.delete(random_note_id)
+        logger.info((status, note))
+        assert status == 0
+
+    def test_trash(self):
+        random_note_id = self.__get_random_note_id()
+        status, note_d = self.API.retrieve(random_note_id)
+        assert status == 0
+        assert isinstance(note_d, dict)
+        status, note = self.API.trash(random_note_id)
+        logger.info((status, note))
         assert status == 0
 
 
