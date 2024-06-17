a8a1831ae04b7a8e84873337413572cf80b1f248
redatman <evicwork@gmail.com>
2024-06-18 01:15:58 +0800

    feat: add the new api.Simplenote

--- api.py
+++ api.py
@@ -8,7 +8,8 @@ import logging
 import os
 import pickle
 import time
-from typing import Dict
+from typing import Any, Dict, Optional
+from urllib.parse import urlencode
 import uuid
 
 from config import CONFIG
@@ -17,6 +18,57 @@ from utils.request import request
 
 
 logger = logging.getLogger()
+
+
+class URL:
+    BASE: str = "https://api.simperium.com/1"
+    DATA = f"{BASE}/{CONFIG.SIMPLENOTE_APP_ID}/{CONFIG.SIMPLENOTE_BUCKET}"
+    __auth = f"{BASE}/{CONFIG.SIMPLENOTE_APP_ID}/authorize/"
+    __index = DATA + "/index"
+    __retrieve = DATA + "/i/%s"
+    __modify = __retrieve
+    __delete = __retrieve
+
+    @classmethod
+    def auth(cls):
+        return cls.__auth
+
+    @classmethod
+    def index(cls, **kwargs: Dict[str, Any]):
+        """
+        e.g. "https://api.simperium.com/1/chalk-bump-f49/note/index?limit=10&data=true"
+        """
+        return cls.__index + "?" + urlencode(kwargs)
+
+    @classmethod
+    def retrieve(cls, note_id: str, version: Optional[int] = None):
+        """
+        e.g. "https://api.simperium.com/1/chalk-bump-f49/note/i/ba4f2735aab811e89fd89d5f0cfefda5"
+        """
+        if version is not None:
+            return cls.__retrieve % note_id + "/v/%s" % version
+        return cls.__retrieve % note_id
+
+    @classmethod
+    def modify(cls, note_id: str, version: Optional[int] = None, response: int = 1, **kwargs: Dict[str, Any]):
+        """
+        e.g. "https://api.simperium.com/1/chalk-bump-f49/note/i/ba4f2735aab811e89fd89d5f0cfefda5/v/2?response=1"
+        """
+        _: str = cls.__modify % note_id
+        params = "?response=%s" % response + urlencode(kwargs)
+        if version is not None:
+            return _ + "/v/%s?" % version + params
+        return _ + params
+
+    @classmethod
+    def delete(cls, note_id: str, version: Optional[int] = None, **kwargs: Dict[str, Any]):
+        """
+        e.g. "https://api.simperium.com/1/chalk-bump-f49/note/i/ba4f2735aab811e89fd89d5f0cfefda5/v/2"
+        """
+        _: str = cls.__delete % note_id
+        if version is not None:
+            return _ + "/v/%s" % version + urlencode(kwargs)
+        return _ + urlencode(kwargs)
 
 
 class SimplenoteLoginFailed(Exception):
@@ -105,6 +157,164 @@ class Simplenote(Singleton):
                 logger.exception(err)
                 raise err
         return self._token
+
+    def index(
+        self,
+        limit: int = CONFIG.NOTE_FETCH_LENGTH,
+        data: bool = False,
+    ):
+        """Method to get the note list
+
+        The method can be passed optional arguments to limit the
+        the list to notes containing a certain tag. If omitted a list
+        of all notes is returned.
+
+        Arguments:
+            - tags=[] list of tags as string: return notes that have
+              at least one of these tags
+
+        Returns:
+            A tuple `(status, notes)`
+
+            - status (int): 0 on success and -1 otherwise
+            - notes (list): A list of note objects with all properties set except `content`.
+            {
+                'current': '666e8b00a5cc2b14e5c85722',
+                'index': [
+                {
+                    'd':
+                    {
+                        'tags': [],
+                        'deleted': False,
+                        'shareURL': '',
+                        'publishURL': '',
+                        'content': '# 1',
+                        'systemTags': [],
+                        'modificationDate': 1718520576.085572,
+                        'creationDate': 1718520569.25186
+                    },
+                    'id': 'd3aa2b8e6ade430bb0bd17e66b20d428',
+                    'v': 2
+                }],
+                'mark': '666e8b00a5cc2b14e5c85722'
+            }
+        """
+        params: Dict[str, Any] = {
+            "limit": limit,
+            # "data": data,
+        }
+        if data:
+            params["data"] = "true"
+
+        try:
+            response = request(
+                URL.index(**params),
+                params=params,
+                headers={self.header: self.token},
+            )
+            return 0, response.data
+        except IOError as err:
+            logger.exception(err)
+        return -1, {}
+
+    def retrieve(self, note_id: str, version: Optional[int] = None):
+        """Method to get a specific note
+
+        Arguments:
+            - note_id (string): ID of the note to get
+            - version (int): optional version of the note to get
+
+        Returns:
+            A tuple `(status, note)`
+
+            - status (int): 0 on success and -1 otherwise
+            - note (dict): note object
+        """
+        try:
+            response = request(
+                URL.retrieve(note_id, version),
+                method="GET",
+                headers={self.header: self.token},
+            )
+            return 0, response.data
+        except IOError as err:
+            logger.exception(err)
+        return -1, {}
+
+    def modify(self, note: Dict[str, Any], note_id: Optional[str] = None, version: Optional[int] = None):
+        """Method to modify or create a note
+
+        Arguments:
+            - note (dict): note object to modify
+            - note_id (string): ID of the note to modify
+            - version (int): optional version of the note to modify
+
+        Returns:
+            A tuple `(status, note)`
+
+            - status (int): 0 on success and -1 otherwise
+            - note (dict): note object
+        """
+        if not isinstance(note, dict):
+            raise ValueError("note should be a string or a dict, but got %s" % note)
+        if note_id is None:
+            note_id = uuid.uuid4().hex
+        try:
+            response = request(
+                URL.modify(note_id, version),
+                method="POST",
+                headers={self.header: self.token},
+                data=note,
+            )
+            return 0, response.data
+        except IOError as err:
+            logger.exception(err)
+        return -1, {}
+
+    def delete(self, note_id: str, version: Optional[int] = None):
+        """Method to permanently delete a note
+
+        Arguments:
+            - note_id (string): key of the note to trash
+
+        Returns:
+            A tuple `(status, note)`
+
+            - status (int): 0 on success and -1 otherwise
+            - note (dict): an empty dict or an error message
+        """
+        try:
+            response = request(
+                URL.delete(note_id, version),
+                method="DELETE",
+                headers={self.header: self.token},
+            )
+            return 0, response.data
+        except IOError as err:
+            logger.exception(err)
+        return -1, {}
+
+    def trash(self, note_id: str, version: Optional[int] = None):
+        """Method to move a note to the trash
+
+        Arguments:
+            - note_id (string): key of the note to trash
+
+        Returns:
+            A tuple `(status, note)`
+
+            - status (int): 0 on success and -1 otherwise
+            - note (dict): the newly created note or an error message
+        """
+        status, note = self.retrieve(note_id, version)
+        if status == -1:
+            return status, note
+        assert isinstance(note, dict), "note is not a dict: %s" % note
+        assert "deleted" in note, "deleted not in note: %s" % note
+        assert isinstance(note["deleted"], bool), "note['deleted'] is not a bool: %s" % note["deleted"]
+        note["deleted"] = True
+        note["modificationDate"] = time.time()
+        return self.modify(note, note_id, version)
 
     def get_note(self, note_id, version=None):
         """Method to get a specific note
