2072f6862a5118666053ef6d856f592392f3da79
redatman <evicwork@gmail.com>
2024-07-30 23:09:41 +0800

    test: Case-sensitivity in settings file path
    The settings file path in the test was incorrectly using a capitalized filename. This commit corrects the case sensitivity, ensuring the test accurately reflects the expected configuration.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,31 +1,17 @@
+from importlib import import_module
 import logging
-from typing import Any
 from unittest import TestCase
 
-from _config import CONFIG
 from utils.tools import Json2Obj as Settings
 
 
+import_module("utils.logger.init")
 logger = logging.getLogger()
-logger.info(CONFIG)
-
-_note_kwargs: dict[str, Any] = {
-    "tags": [],
-    "deleted": False,
-    "systemTags": [],
-    "content": "SimplenoteTitle\n\n\nSimplenoteBody",
-}
-
-_d_kwargs: dict[str, Any] = {
-    "d": _note_kwargs,
-    "id": "123abc",
-    "v": 2,
-}
 
 
 class TestSettings(TestCase):
     def test_settings(self):
-        settings = Settings("Simplenote.sublime-settings")
+        settings = Settings("simplenote.sublime-settings")
         logger.info(settings)
         logger.info(settings.username)
         logger.info(settings.get("username"))
