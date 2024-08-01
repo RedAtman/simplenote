6fbdeff55914f75d01f94ff310121fe6331753d6
redatman <evicwork@gmail.com>
2024-08-01 20:51:03 +0800

    fix: Add  settings template
    Adds a new template for configuring Simplenote's settings. This allows users to customize how the plugin interacts with their Simplenote account.

--- _config.py
+++ _config.py
@@ -24,6 +24,55 @@ __all__ = [
 ]
 
 
+_SETTINGS_TEMPLATE = """
+// Simplenote Settings - User
+// First time use, To configure your Simplenote account username and password in the settings file.
+// After that, save the settings file, Then wait for the sync.
+{
+    // --------------------------------
+    // Credentials:
+    // --------------------------------
+    "username": ""
+    ,"password": ""
+    // --------------------------------
+    // Sync settings:
+    // --------------------------------
+    // Sync when sublime text starts:
+    ,"autostart": true
+    // Sync automatically (in seconds)
+    ,"sync_every": 30
+    // Number of notes synchronized each time
+    ,"sync_note_number": 1000
+    // Conflict resolution (If a file was edited on another client and also here, on sync..)
+    // Server Wins (Same as selecting 'Overwrite')
+    ,"on_conflict_use_server": false
+    // Local is left unchanged (Same as selecting 'Cancel')
+    ,"on_conflict_leave_alone": false
+    // --------------------------------
+    // Autosave (beta)
+    // --------------------------------
+    // Activate autosave and tell how much (in seconds) to wait
+    // after you stop typing to send the save
+    ,"autosave_debounce_time": 1
+    // --------------------------------
+    // File extension support
+    // --------------------------------
+    // Which file extension should the temporal files use?
+    // This allows you to interact with other plugins such as
+    // PlainTasks defining an extension for certain note title
+    ,"title_extension_map": [{
+        "title_regex": "[ST]"
+        ,"extension": "todo"
+    },
+    {
+        "title_regex": "# "
+        ,"extension": "md"
+        ,"systemTags": ["markdown"]
+    }]
+}
+"""
+
+
 class _BaseConfig:
     # All subclasses of BaseConfig will be added to this mapping.
     mapping: typing.Dict[str, type] = {}
@@ -39,7 +88,7 @@ class _BaseConfig:
     sys.path.insert(0, BASE_DIR)
     # LOG_DIR: str = os.path.join(BASE_DIR, "logs")
     # os.makedirs(LOG_DIR, exist_ok=True)
-    LOG_LEVEL: str = os.getenv("LOG_LEVEL", "INFO")
+    LOG_LEVEL: str = os.getenv("LOG_LEVEL", "WARNING")
 
     SIMPLENOTE_PROJECT_NAME: str = "Simplenote"
     SIMPLENOTE_PROJECT_VERSION: str = "0.0.1"
@@ -64,12 +113,18 @@ class _BaseConfig:
 
     SUBLIME_USER_DIR = os.path.join(packages_path(), "User")
     SIMPLENOTE_SETTINGS_FILE_PATH = SIMPLENOTE_SETTINGS_FILE
-    # SIMPLENOTE_SETTINGS_FILE_PATH = os.path.join(SUBLIME_USER_DIR, SIMPLENOTE_SETTINGS_FILE)
-    # if not os.path.exists(SIMPLENOTE_SETTINGS_FILE_PATH):
-    #     import shutil
+    SIMPLENOTE_SETTINGS_USER_FILE_PATH = os.path.join(SUBLIME_USER_DIR, SIMPLENOTE_SETTINGS_FILE)
+    SETTINGS_TEMPLATE: str = _SETTINGS_TEMPLATE
+    if not os.path.exists(SIMPLENOTE_SETTINGS_USER_FILE_PATH):
+        try:
+            with open(SIMPLENOTE_SETTINGS_USER_FILE_PATH, "r") as f:
+                import json
 
-    #     default_settings = os.path.join(os.path.dirname(__file__), SIMPLENOTE_SETTINGS_FILE)
-    #     shutil.copy(default_settings, SIMPLENOTE_SETTINGS_FILE_PATH)
+                _ = json.loads(f.read())
+        except Exception as e:
+            print(e)
+            with open(SIMPLENOTE_SETTINGS_USER_FILE_PATH, "w+") as f:
+                f.write(SETTINGS_TEMPLATE)
 
     SIMPLENOTE_CACHE_DIR = os.path.join(cache_path(), SIMPLENOTE_PROJECT_NAME)
     os.makedirs(SIMPLENOTE_CACHE_DIR, exist_ok=True)
@@ -104,12 +159,12 @@ env = os.getenv("ENV", "development")
 CONFIG: typing.Type[_BaseConfig] = _BaseConfig.mapping.get(env, Development)
 
 # Set environment variables
-for attr in dir(CONFIG):
-    if attr.startswith("_"):
-        continue
-    if attr.startswith(("SIMPLENOTE", "LOG_LEVEL")):
-        os.environ[attr] = str(getattr(CONFIG, attr))
-        # print("set %s: %s" % (attr, getattr(CONFIG, attr)))
+# for attr in dir(CONFIG):
+#     if attr.startswith("_"):
+#         continue
+#     if attr.startswith(("SIMPLENOTE", "LOG_LEVEL")):
+#         os.environ[attr] = str(getattr(CONFIG, attr))
+# print("set %s: %s" % (attr, getattr(CONFIG, attr)))
 
 
 if __name__ == "Simplenote._config":
--- lib/gui.py
+++ lib/gui.py
@@ -23,75 +23,9 @@ __all__ = [
     "on_note_changed",
 ]
 
-settings_content = """
-{
-    // --------------------------------
-    // Credentials:
-    // --------------------------------
-    "username": ""
-    ,"password": ""
-    // --------------------------------
-    // Sync settings:
-    // --------------------------------
-    // Sync when sublime text starts:
-    ,"autostart": true
-    // Sync automatically (in seconds)
-    ,"sync_every": 30
-    // Number of notes synchronized each time
-    ,"sync_note_number": 1000
-    // Conflict resolution (If a file was edited on another client and also here, on sync..)
-    // Server Wins (Same as selecting 'Overwrite')
-    ,"on_conflict_use_server": false
-    // Local is left unchanged (Same as selecting 'Cancel')
-    ,"on_conflict_leave_alone": false
-    // --------------------------------
-    // Autosave (beta)
-    // --------------------------------
-    // Activate autosave and tell how much (in seconds) to wait
-    // after you stop typing to send the save
-    ,"autosave_debounce_time": 1
-    // --------------------------------
-    // File extension support
-    // --------------------------------
-    // Which file extension should the temporal files use?
-    // This allows you to interact with other plugins such as
-    // PlainTasks defining an extension for certain note title
-    ,"title_extension_map": [{
-        "title_regex": "\\[ST\\]"
-        ,"extension": "todo"
-    },
-    {
-        "title_regex": "\\# "
-        ,"extension": "md"
-        ,"systemTags": ["markdown"]
-    }]
-}
-"""
-
 
 def edit_settings():
-    installed_package_settings_file = os.path.join(
-        CONFIG.SIMPLENOTE_INSTALLED_PACKAGE_DIR, CONFIG.SIMPLENOTE_SETTINGS_FILE_PATH
-    )
-    package_settings_file = os.path.join(CONFIG.SIMPLENOTE_PACKAGE_DIR, CONFIG.SIMPLENOTE_SETTINGS_FILE_PATH)
-    logger.warning((os.path.exists(installed_package_settings_file), installed_package_settings_file))
-    logger.warning((os.path.exists(package_settings_file), package_settings_file))
-    if os.path.exists(installed_package_settings_file):
-        settings_file = installed_package_settings_file
-    elif os.path.exists(package_settings_file):
-        settings_file = package_settings_file
-    else:
-        settings_file = CONFIG.SIMPLENOTE_SETTINGS_FILE_PATH
-
-    # logger.warning(("CONFIG.SIMPLENOTE_SETTINGS_FILE_PATH", CONFIG.SIMPLENOTE_SETTINGS_FILE_PATH))
-
-    # settings_file = os.path.join(
-    #     sublime.installed_packages_path(), "Simplenote.sublime-package", CONFIG.SIMPLENOTE_SETTINGS_FILE_PATH
-    # )
-    # settings_file = (
-    #     """/Users/nut/Library/Application Support/Sublime Text/Packages/Simplenote/Simplenote.sublime-settings"""
-    # )
-    # settings_file = os.path.join(os.path.dirname(__file__), settings_file)
+    settings_file = CONFIG.SIMPLENOTE_SETTINGS_USER_FILE_PATH
     logger.warning((type(settings_file), settings_file))
     # with open(settings_file) as f:
     #     settings = f.read()
@@ -99,7 +33,10 @@ def edit_settings():
     # sublime.run_command("open_file", {"file": settings_file})
     sublime.run_command(
         "edit_settings",
-        {"base_file": settings_file, "default": "// Simplenote Settings - User\n{\n\t$0\n}\n" + settings_content},
+        {
+            "base_file": settings_file,
+            # "default": "// Simplenote Settings - User\n{\n\t$0\n}\n",
+        },
     )
 
 
